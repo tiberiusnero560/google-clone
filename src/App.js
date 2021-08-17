a1a0841f6b6307d57e6661a30cec97edbd5feee9
Kyle LeBlanc <53625838+tiberiusnero560@users.noreply.github.com>
2021-08-17 19:38:18 -0400

    Good progress
    Made some good progress. Finished the top header and the links.
    
    Working on the body now and I have the Google logo. Starting to take shape.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from "react";
 import './App.css';
-import Home from './pages/Home';
+import Home from './Home';
+import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <h1>This is the h1 from the app.js component</h1>
+      <Router>
+        
+        <Switch>
+          <Route path="/search">
+            <h1> This is the search page </h1>
+          </Route>
+
+          <Route path="/">
+            <Home />
+            <h1> This is the home page</h1>
+          </Route>
+        </Switch>
+
+      
+      </Router>
 
 
-    {/* Home page with Search bar */}
-    <Home />
-
-
-    {/* Search results page */}
+     
 
 
     </div>
---
+++ src/Home.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Home.css'
+import  { Link } from "react-router-dom";
+import AppsIcon from "@material-ui/icons/Apps";
+import Avatar from '@material-ui/core/Avatar';
+
+// The Homepage component
+
+function Home() {
+    return (
+        <div className='home'>
+
+            {/* The header with About, Store, gmail, images and profile    */}
+            <div className='home__header'>
+                <div className='home__headerLeft'>
+                    <Link to='/about'>about</Link>
+                    <Link to='/store'>Store</Link>
+                </div>
+
+                <div className='home__headerRight'>
+                    <Link to='./gmail'>Gmail</Link>
+                    <Link to='./images'>Images</Link>  
+                    <AppsIcon />                 
+                    <Avatar />
+
+                </div>
+            </div>
+
+
+            {/*  The body of items below the header */}
+            <div className='home__body'>
+
+                <img 
+                    src="http:///www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt=""
+                />
+                    
+                
+
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Home
