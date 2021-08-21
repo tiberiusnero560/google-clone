fafb8bd95aa0b497d071dd9fa64b2dfb0f82c044
Kyle LeBlanc <53625838+tiberiusnero560@users.noreply.github.com>
2021-08-21 17:10:41 -0400

    Successful Search Functionality
    The google custom search API is working correctly. It redirects to the search page, and it returns a json object with the
    context, items, kind, queries, searchinfo, url.
    Next step is to style and design the search page using this information.

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,10 +1,17 @@
 import React from 'react'
-
 import { useStateValue } from "./StateProvider";
+import useGoogleSearch from './useGoogleSearch';
+import response from './response';
 
 function SearchPage() {
     const [{ term }, dispatch] = useStateValue();
+    // LIVE API CALL
+    // const { data } = useGoogleSearch(term);             // Here we are using the data object we returned in the useGoogleSearch custom hook with the term we entered and stored in data layer  
 
+    // OFFLINE CALL using json response from first call
+    const data = Response;
+
+    console.log(data)
     return (
         <div className='searchPage'>
             <div className='searchPage_header'>
@@ -20,3 +27,5 @@ function SearchPage() {
 
 export default SearchPage
     
+
+
--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -4,15 +4,31 @@ import API_KEY from "./keys";
 
 const CONTEXT_KEY = "e1d05a8fe2e08f2ab";
 
+
 // Custom hook
 // We pass in term, and when the term changes, it fires off the useEffect
 const useGoogleSearch = (term) => {
     const [data, setData] = useState(null);
  
     useEffect(() => {
+        // We want to use an asynchronise fetch inside the useEffect
+        const fetchData = async() => {
+            fetch(
+                `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+            )
+            .then(response => response.json())       // Get the Json from the response
+            .then(result => {
+                setData(result)
+            })
+        }
+        fetchData();
+    }, [term]) // term is the data layer variable
 
-    }, [term])
-
+    return { data }   // Here we return the data, and then we use it or display it on the SearchPage
+    // This returns an object, This useGoogleSearch is a custom hook. useEffect is a hook, and we just made our own.
 };
+
+// As a note. We are using fetch here, but once I am more familiar with it, using Axios can be a powerful upgrade
+
 
 export default useGoogleSearch;
